0fa7c3e0b7b934d945b63d7eb24bdd5377c0aad7
Justin Anagonye <justinanagonye@justins-mbp-3.devices.brown.edu>
2022-03-18 13:47:19 -0400

    Done

---
+++ reactlabapp/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Horoscope from './Horoscope';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <Horoscope/>
+        </header>
+      </div>
+  );
+}
+
+export default App;
